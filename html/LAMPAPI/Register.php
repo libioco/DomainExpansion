eb5e6c863393f472169ea1379c48a715020b0f33
hgorak <55695914+hgorak@users.noreply.github.com>
2023-09-04 15:51:57 -0400

    Update Register.php #2
    scrapped previous register.php, still throwing a 505 but have yet to fix it

--- html/LAMPAPI/Register.php
+++ html/LAMPAPI/Register.php
@@ -2,14 +2,10 @@
 
         $inData = getRequestInfo();
 
-        $id = 0;
-        $firstName = "";
-        $lastName = "";
-        $login = "";
-        $password = "";
-
-        $searchResults = "";
-        $searchCount = 3;
+        $firstName = $inData["firstName"];
+        $lastName = $inData["lastName"];
+        $login = $inData["login"];
+        $password = $inData["password"];
 
         $conn = new mysqli("localhost", "domain", "expansion", "COP4331");
         if ($conn->connect_error)
@@ -18,35 +14,12 @@
         }
         else
         {
-                $stmt = $conn->prepare("select * from User where FirstName like ? and UserID=?");
-                $firstName = "%" . $inData["search"] . "%";
-                $stmt->bind_param("ss", $firstName, $inData["userId"]);
+                $stmt = $conn->prepare("INSERT into User (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+                $stmt->bind_param("ss", $firstName, $lastName, $login, $password);
                 $stmt->execute();
-
-                $result = $stmt->get_result();
-
-                while($row = $result->fetch_assoc())
-                {
-                        if( $searchCount > 3 )
-                        {
-                                $searchResults .= ",";
-                        }
-                        $searchCount++;
-                        $searchResults .= '{"FirstName" : "' . $row{"FirstName"}. '"}';
-                }
-
-                if( $searchCount == 3 )
-                {
-                        returnWithError( "No Records Found" );
-                }
-                else
-                {
-                        $id = $searchCount++;
-                        returnWithInfo( $searchResults );
-                }
-
                 $stmt->close();
                 $conn->close();
+                returnWithError("");
         }
 
         function getRequestInfo()
@@ -62,15 +35,8 @@
 
         function returnWithError( $err )
         {
-                $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-                sendResultInfoAsJson( $retValue );
-        }
-
-        function returnWithInfo( $searchResults )
-        {
-                $retValue = '{"results":[' . $searchResults . '],"error":""}';
+                $retValue = '{"error":"' . $err . '"}';
                 sendResultInfoAsJson( $retValue );
         }
 
 ?>
-
