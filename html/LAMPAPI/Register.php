ae4ed93d3a71a675fed34aabf02da7fef55d394d
hgorak <55695914+hgorak@users.noreply.github.com>
2023-09-04 15:19:14 -0400

    Update Register.php
    Adding a search function so that we can automatically increment our ID numbers for new users

--- html/LAMPAPI/Register.php
+++ html/LAMPAPI/Register.php
@@ -1,3 +1,76 @@
 <?php
 
+        $inData = getRequestInfo();
+
+        $id = 0;
+        $firstName = "";
+        $lastName = "";
+        $login = "";
+        $password = "";
+
+        $searchResults = "";
+        $searchCount = 3;
+
+        $conn = new mysqli("localhost", "domain", "expansion", "COP4331");
+        if ($conn->connect_error)
+        {
+                returnWithError( $conn->connect_error );
+        }
+        else
+        {
+                $stmt = $conn->prepare("select * from User where FirstName like ? and UserID=?");
+                $firstName = "%" . $inData["search"] . "%";
+                $stmt->bind_param("ss", $firstName, $inData["userId"]);
+                $stmt->execute();
+
+                $result = $stmt->get_result();
+
+                while($row = $result->fetch_assoc())
+                {
+                        if( $searchCount > 3 )
+                        {
+                                $searchResults .= ",";
+                        }
+                        $searchCount++;
+                        $searchResults .= '{"FirstName" : "' . $row{"FirstName"}. '"}';
+                }
+
+                if( $searchCount == 3 )
+                {
+                        returnWithError( "No Records Found" );
+                }
+                else
+                {
+                        $id = $searchCount++;
+                        returnWithInfo( $searchResults );
+                }
+
+                $stmt->close();
+                $conn->close();
+        }
+
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
+
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+
+        function returnWithError( $err )
+        {
+                $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
+
+        function returnWithInfo( $searchResults )
+        {
+                $retValue = '{"results":[' . $searchResults . '],"error":""}';
+                sendResultInfoAsJson( $retValue );
+        }
+
 ?>
+
