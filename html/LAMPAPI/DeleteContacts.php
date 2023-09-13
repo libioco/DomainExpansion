37efedca1d046e3cbba76930bb8a877cd643c926
hgorak <55695914+hgorak@users.noreply.github.com>
2023-09-13 19:13:03 -0400

    Create DeleteContacts.php

---
+++ html/LAMPAPI/DeleteContacts.php
@@ -0,0 +1,40 @@
+
+<?php
+        $inData = getRequestInfo();
+
+        $userId = $inData["userId"];
+        $contactId = $inData["contactId"];
+
+        $conn = new mysqli("localhost", "domain", "expansion", "COP4331");
+        if ($conn->connect_error)
+        {
+                returnWithError( $conn->connect_error );
+        }
+        else
+        {
+                $stmt = $conn->prepare("DELETE FROM Contact WHERE UserID = ? AND ContactID = ?;");
+                $stmt->bind_param("ss", $userId, $contactId);
+                $stmt->execute();
+                $stmt->close();
+                $conn->close();
+                returnWithError("");
+        }
+
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
+
+        function sendResultInfoAsJson( $obj )
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+
+        function returnWithError( $err )
+        {
+                $retValue = '{"error":"' . $err . '"}';
+                sendResultInfoAsJson( $retValue );
+        }
+
+?>
